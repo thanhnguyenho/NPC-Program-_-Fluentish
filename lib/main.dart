d8578b3509641b09d28095f577d040283671442e
maryangelene <mary.taylor@students.mq.edu.au>
2026-07-04 11:50:28 +0700

    feat: added soundboard page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'src/features/soundboard/soundboard_page.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Fluentish'),
+      home: const SoundboardPage(),
     );
   }
 }
--- lib/src/features/soundboard/soundboard_page.dart
+++ lib/src/features/soundboard/soundboard_page.dart
@@ -1,1 +1,18 @@
-// TODO: Implement Soundboard page.
+import 'package:flutter/material.dart';
+
+class SoundboardPage extends StatelessWidget {
+  const SoundboardPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFEEDADA),
+      body: const Center(
+        child: Text(
+          'Soundboard Page',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
