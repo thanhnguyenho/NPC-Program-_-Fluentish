256fe1f63c67126594cb5e3e840643539d2922be
ChrisCrowne <christopher.crowne@students.mq.edu.au>
2026-07-16 09:59:11 +0700

    Remove unused flutter/material import
    The import of 'package:flutter/material.dart' was not being used in auth_service.dart. Removing it reduces unnecessary dependencies and improves code cleanliness.

--- lib/src/shared/services/auth_service.dart
+++ lib/src/shared/services/auth_service.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/material.dart';
 
 class Auth {
   //allows app to interact with firebase
